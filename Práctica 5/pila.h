9ac787258887a99237659e0e4c57340b67e53830
Kv20ion <140846149+Kv20ion@users.noreply.github.com>
2026-03-17 16:11:29 -0600

    Update pila.h

--- Práctica 5/pila.h
+++ Práctica 5/pila.h
@@ -1,26 +1,36 @@
+/*
+ * ELABORADO POR: BRIGADA #1
+ *
+ * ESPINOSA SALVADOR ROMAN
+ * CASTELÁN SIERRA GAEL
+ * MALDONADO MARTÍNEZ ERICK FERNANDO
+ * VIDAURE ÁLVAREZ KEVIN EMMANUEL
+ *
+ */
+
 #ifndef PILA_H
 #define PILA_H
 
-/* Declaraciones adelantadas */
-typedef struct Nodo
-{
-	int dato;
-	struct Nodo *sig;
-} Nodo;
+// Estructura del dato a almacenar
+typedef struct {
+    int valor;
+    char descripcion[100];
+} ElementoPila;
 
-typedef struct Pila
-{
-	Nodo *tope;
-	int tam; /* Ahora el tamaño es un entero normal */
+typedef struct Nodo Nodo;
+
+typedef struct Pila {
+    Nodo *tope;
+    int tam;
 } Pila;
 
-/* Funciones */
+// Inicialización de las funciones
 void pila_init(Pila *p);
-int pila_esta_vacia(Pila *p);
+int  pila_esta_vacia(Pila *p);
 
-int pila_push(Pila *p, int valor);
-int pila_pop(Pila *p, int *salida);
-int pila_peek(Pila *p, int *salida);
+int  pila_push(Pila *p, ElementoPila valor);
+int  pila_pop(Pila *p, ElementoPila *salida);
+int  pila_peek(Pila *p, ElementoPila *salida);
 
 void pila_clear(Pila *p);
 void pila_destroy(Pila *p);
