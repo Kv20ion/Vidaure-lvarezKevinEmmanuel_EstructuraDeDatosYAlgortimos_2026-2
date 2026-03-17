568aeac17773445e3acf439dbfe7e2e50ce1d0e6
Kv20ion <140846149+Kv20ion@users.noreply.github.com>
2026-03-17 16:09:34 -0600

    Update cola.h

--- Práctica 5/cola.h
+++ Práctica 5/cola.h
@@ -1,21 +1,38 @@
+/*
+ * ELABORADO POR: BRIGADA #1
+ *
+ * ESPINOSA SALVADOR ROMAN
+ * CASTELÁN SIERRA GAEL
+ * MALDONADO MARTÍNEZ ERICK FERNANDO
+ * VIDAURE ÁLVAREZ KEVIN EMMANUEL
+ *
+ */
+
 #ifndef COLA_H
 #define COLA_H
+
+// Estructura de elementos
+typedef struct {
+    int numero;
+    char texto[100]; // Cadena de caracteres
+} ElementoCola;
 
 typedef struct Nodo Nodo;
 
 typedef struct Cola { // Cola FIFO
-  Nodo *frente; // Cabeza, el primer elemento
-  Nodo *final; // Cola, el ultimo elemento
-  int tam; // Tamanio de la cola
+    Nodo *frente; // Primer elemento
+    Nodo *final; // Último elemento
+    int tam; // Tamaño de la cola
 } Cola;
 
+// Inicialización de funciones
 void inicializar(Cola *c);
-void encolar(Cola *c, int v);
-int desencolar(Cola *c);
-int colaVacia(Cola *c);
-int colaTamanio(Cola *c);
+void encolar(Cola *c, ElementoCola v);
+int  desencolar(Cola *c, ElementoCola *salida);
+int  colaVacia(Cola *c);
+int  colaTamanio(Cola *c);
 void limpiarCola(Cola *c);
 void destruirCola(Cola *c);
-int colaFrente(Cola *c);
+int  colaFrente(Cola *c, ElementoCola *salida);
 
-#endif // COLA_H
+#endif
